791787eb3f440f89335d1ef8613fa952982092b0
trkhduy <trkhduy003@gmail.com>
2024-01-13 21:23:43 +0700

    first commit

--- src/main/java/com/example/springproject/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.springproject.exception;
-
-import com.example.springproject.exception.base.NotFoundException;
-
-public class UserNotFoundException extends NotFoundException {
-  public UserNotFoundException() {
-    setCode("com.example.springproject.exception.base.NotFoundException.UserNotFoundException");
-  }
-}
