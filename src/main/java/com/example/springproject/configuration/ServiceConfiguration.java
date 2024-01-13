641378d90b40802835a27fa4c59d2a4a4ff25426
trkhduy <trkhduy003@gmail.com>
2024-01-13 21:31:09 +0700

    new base

--- src/main/java/com/example/springproject/configuration/ServiceConfiguration.java
+++ src/main/java/com/example/springproject/configuration/ServiceConfiguration.java
@@ -1,11 +1,8 @@
 package com.example.springproject.configuration;
 
 
-import com.example.springproject.repository.UserRepository;
 import com.example.springproject.service.base.MessageService;
-import com.example.springproject.service.UserService;
 import com.example.springproject.service.base.MessageServiceImpl;
-import com.example.springproject.service.impl.UserServiceImpl;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +12,6 @@ import org.springframework.context.annotation.Bean;
  */
 @Configuration
 public class ServiceConfiguration {
-  @Bean
-  public UserService userService(UserRepository repository) {
-    return new UserServiceImpl(repository);
-  }
 
   @Bean
   public MessageService messageService(MessageSource messageSource) {
--- src/main/java/com/example/springproject/constant/MessageCodeConstant.java
+++ src/main/java/com/example/springproject/constant/MessageCodeConstant.java
@@ -3,9 +3,6 @@ package com.example.springproject.constant;
  * This is Message Code Constant
  */
 public class MessageCodeConstant {
-  public static final String GET_USER_BY_ID = "com.example.springproject.controller.detail";
-  public static final String CREATE_USER = "com.example.springproject.controller.create";
-  public static final String LIST_USER = "com.example.springproject.controller.list";
-  public static final String DELETE_USER = "com.example.springproject.controller.delete";
+
 
 }
